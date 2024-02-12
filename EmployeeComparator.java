ba950aa33d565a3b402104288534bb45af9e9741
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-12 22:27:46 +0530

    Update EmployeeComparator.java

--- EmployeeComparator.java
+++ EmployeeComparator.java
@@ -1,27 +1,28 @@
-package net.texala.csv_file.employee_comparator;
+package net.texala.main;
 
 import java.util.Comparator;
 
-import net.texala.csv_file.employee.Employee;
-
 public class EmployeeComparator implements Comparator<Employee> {
-	private String sortBy;
+    private String sortBy;
 
-	public EmployeeComparator(String sortBy) {
-		this.sortBy = sortBy;
-	}
+    public EmployeeComparator(String sortBy) {
+        this.sortBy = sortBy;
+    }
 
-	@Override
-	public int compare(Employee emp1, Employee emp2) {
-		switch (sortBy) {
-		case "firstName":
-			return emp1.getFirstName().compareTo(emp2.getFirstName());
-		case "lastName":
-			return emp1.getLastName().compareTo(emp2.getLastName());
-		case "empID":
-			return Integer.compare(emp1.getEmpID(), emp2.getEmpID());
-		default:
-			return 0;
-		}
-	}
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        switch (sortBy.toLowerCase()) {
+            case "firstname":
+                return emp1.getFirstName().compareToIgnoreCase(emp2.getFirstName());
+            case "lastname":
+                return emp1.getLastName().compareToIgnoreCase(emp2.getLastName());
+            case "empid":
+                return Integer.compare(emp1.getEmpID(), emp2.getEmpID());
+            case "department":
+                return emp1.getDepartment().compareToIgnoreCase(emp2.getDepartment());
+            default:
+                throw new IllegalArgumentException("Invalid sorting criteria: " + sortBy);
+        }
+    }
+
 }
